de5d515307a1f742c83641fc4e8f5b95f249b620
TOKMAKA <wzl3258224377@163.com>
2026-06-15 12:04:13 +0800

    changed eee

--- file1/multiplexer.vhd
+++ file1/multiplexer.vhd
@@ -19,7 +19,7 @@ Y : out MVL7_VECTOR(11 downto 0)
 );
 end multiplexer;
 
-////////////////////architecture body ddd////////////////////
+////////////////////architecture body eee////////////////////
 
 architecture mux of multiplexer is 
 begin 
