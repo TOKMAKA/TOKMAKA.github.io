ee3903641aaca1a585a8fcdac1d4c46988a28e09
TOKMAKA <wzl3258224377@163.com>
2026-06-15 19:35:29 +0800

    changed explaination ggg

--- file1/multiplexer.vhd
+++ file1/multiplexer.vhd
@@ -19,7 +19,7 @@ Y : out MVL7_VECTOR(11 downto 0)
 );
 end multiplexer;
 
-////////////////////architecture body fff////////////////////
+////////////////////architecture body ggg////////////////////
 
 architecture mux of multiplexer is 
 begin 
